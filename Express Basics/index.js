ef269401058dd953035e0f7c048fe2692d544e44
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-05 13:40:57 +0530

    practise

--- Express Basics/index.js
+++ Express Basics/index.js
@@ -4,6 +4,22 @@ import data from "./data/mock.json";
 const app = express();
 const PORT = 5000;
 
+app.get("/", (req, res) => {
+	res.send("This is a GET request at /");
+});
+
+app.post("/create", (req, res) => {
+	res.send("This is a POST request at /create");
+});
+
+app.put("/update", (req, res) => {
+	res.send("This is a PUT request at /update");
+});
+
+app.delete("/delete", (req, res) => {
+	res.send("This is a DELETE request at /delete");
+});
+
 app.listen(PORT, () => {
 	console.log("The server is running on port:", PORT);
 	console.log("Mock data", data);
