96fd55a6ea2164efae524fbd3cfdfe13c1be5ae1
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-05 14:13:52 +0530

    practise

--- index.js
+++ index.js
@@ -4,29 +4,49 @@ import data from "./data/mock.json";
 const app = express();
 const PORT = 5000;
 
+//public folder
 app.use(express.static("public"));
 
+//images folder
 app.use("/images", express.static("images"));
 
+//GET
 app.get("/", (req, res) => {
 	res.json(data);
 });
 
-//GET with request params
+//GET with routing parameters
 app.get("/class/:id", (req, res) => {
 	const studentId = Number(req.params.id);
 	const student = data.filter((student) => student.id === studentId);
 	res.send(student);
 });
 
+//Get with next
+app.get(
+	"/next/:id",
+	(req, res, next) => {
+		console.log("Routed to next function");
+		next();
+	},
+	(req, res) => {
+		const studentId = Number(req.params.id);
+		const student = data.filter((student) => student.id === studentId);
+		res.send(student);
+	}
+);
+
+//POST
 app.post("/create", (req, res) => {
 	res.send("This is a POST request at /create");
 });
 
+//UPDATE
 app.put("/update", (req, res) => {
 	res.send("This is a PUT request at /update");
 });
 
+//DELETE
 app.delete("/delete", (req, res) => {
 	res.send("This is a DELETE request at /delete");
 });
