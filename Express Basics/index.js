fca62dae0e70e1aea529c1204646a2c6a9c7531d
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-05 13:30:10 +0530

    practise

--- Express Basics/index.js
+++ Express Basics/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import data from "./data/mock.json";
 
 const app = express();
 const PORT = 5000;
 
 app.listen(PORT, () => {
 	console.log("The server is running on port:", PORT);
+	console.log("Mock data", data);
 });
